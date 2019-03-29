dd378a22df59906306a988f695197e3119e30b42
Rubin Ally <ALLAHM007@myuct.ac.za>
2019-03-29 12:02:32 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { CVComponent } from './cv/cv.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatChipsModule, MatChipList } from '@angular/material';
+import { GithubComponent } from './github/github.component';
+
+const routes =[
+  {path: 'Home', component: HomeComponent, useAsDefault: true},
+  { path: 'CV', component: CVComponent },
+  { path: 'Blog', component: BlogComponent },
+  { path: '**', redirectTo: 'Home' }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    CVComponent,
+    GithubComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
